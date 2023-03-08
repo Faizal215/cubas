ce66e4422dac33d0373cdf7c6c60cc9fbab52a17
Faizal215 <127084108+Faizal215@users.noreply.github.com>
2023-03-08 15:48:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,29 @@
-# -*- coding: utf-8 -*-
-#"""
-#Created on Mon Aug  3 21:11:12 2020
-#
-#@author: pattn
-#"""
-
-
-# coding: utf-8
-
 import pandas as pd
+framingham = pd.read_csv('https://raw.githubusercontent.com/Natassha/streamlit_fhs/main/framingham.csv')# Dropping null values
+framingham = framingham.dropna()
+framingham.head()
+framingham['TenYearCHD'].value_counts()
+
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier 
-from sklearn import metrics
-from flask import Flask, request, render_template
-import pickle
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_scoreX = framingham.drop('TenYearCHD',axis=1)
+y = framingham['TenYearCHD']
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.20)oversample = RandomOverSampler(sampling_strategy='minority')
+X_over, y_over = oversample.fit_resample(X_train,y_train)rf = RandomForestClassifier()
+rf.fit(X_over,y_over)
 
-app = Flask("__name__")
+preds = rf.predict(X_test)
+print(accuracy_score(y_test,preds))
 
+import joblib
+joblib.dump(rf, 'fhs_rf_model.pkl')
 
+import streamlit as st
+import joblib
+import pandas as pd
 
-q = ""
+st.write("# 10 Year Heart Disease Prediction")
 
-@app.route("/")
-def loadPage():
-	return render_template('home.html', query="")
-
-
-@app.route("/predict", methods=['POST'])
-def predict():
-    
-    inputQuery1 = request.form['query1']
-    inputQuery2 = request.form['query2']
-    inputQuery3 = request.form['query3']
-    inputQuery4 = request.form['query4']
-    inputQuery5 = request.form['query5']
-
-    model = pickle.load(open("breastCancerModel.pkl", "rb"))
-    
-    
-    data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5]]
-    new_df = pd.DataFrame(data, columns = ['texture_mean', 'perimeter_mean', 'smoothness_mean', 'compactness_mean', 'symmetry_mean'])
-    
-    single = model.predict(new_df)
-    probablity = model.predict_proba(new_df)[:,1]
-    
-    if single==1:
-        o1 = "The patient is diagnosed with Breast Cancer"
-        o2 = "Confidence: {}".format(probablity*100)
-    else:
-        o1 = "The patient is not diagnosed with Breast Cancer"
-        o2 = "Confidence: {}".format(probablity*100)
-        
-    return render_template('home.html', output1=o1, output2=o2, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'])
-    
-if __name__ == "__main__":
-    app.run()
-
+streamlit run streamlit_fhs.py
